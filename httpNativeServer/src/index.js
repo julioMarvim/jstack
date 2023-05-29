61b56a3de29581817df8e3f01def9fb430fbd4cb
julioMarvim <juliomarvim@yahoo.com.br>
2023-05-29 10:41:16 -0300

    feat: trabalhando com url.parse

--- httpNativeServer/src/index.js
+++ httpNativeServer/src/index.js
@@ -1,21 +1,28 @@
 /* Começamos importando o modulo nativo: */
 const http = require('http');
 const routes = require('./routes');
+const url = require('url');
 
 /* Iniciando a constução do servidor */
 const server = http.createServer((request, response) => {
+  
+  /* Utilizando este metodo e o segundo parametro 'parseQueryString' = true
+  conseguimos transformar os queryParams em um objeto, pq por padrão ele é uma string. */
+  const parsedUrl = url.parse(request.url, true);
+
   /* Neste momento estou logando o metodo http e a url. */
-  console.log(`Request method: ${request.method} | endpoint: ${request.url}`);
+  console.log(`Request method: ${request.method} | endpoint: ${parsedUrl.pathname}`);
 
   const route = routes.find((routeObj) => (
-    routeObj.endpoint === request.url && routeObj.method === request.method
+    routeObj.endpoint === parsedUrl.pathname && routeObj.method === request.method
   ));
 
   if(route){
+    request.query = parsedUrl.query;
     route.handler(request, response);
   }else {
     response.writeHead(404, {'Content-Type': 'text/html'});
-    response.end(`Canot ${request.method} ${request.url}`);
+    response.end(`Cannot ${request.method}  ${parsedUrl.pathname}`);
   }
 });
 
