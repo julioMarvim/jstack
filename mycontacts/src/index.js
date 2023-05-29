0c6b89da85ddc1b92401dcfbe3335e5fb58ed699
julioMarvim <juliomarvim@yahoo.com.br>
2023-05-29 18:11:36 -0300

    feat: cria contacts controller

--- mycontacts/src/index.js
+++ mycontacts/src/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
+const routes = require('./routes');
 
 const app = express();
-app.get('/', (request, response) => {
-  response.send('Hello workd!');
-});
+app.use(routes);
 
 app.listen(3000, () => console.log('Server started at http://localhost:3000'));
