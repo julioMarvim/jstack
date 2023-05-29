6d60993d940c6daa1749ffda809aa932976cc48e
julioMarvim <juliomarvim@yahoo.com.br>
2023-05-29 15:08:57 -0300

    feat: atualiza e deleta usuario

--- httpNativeServer/src/controllers/UserController.js
+++ httpNativeServer/src/controllers/UserController.js
@@ -1,5 +1,5 @@
 /* Iremos criar um CRUD de usuários. */
-const users = require('../mocks/users');
+let users = require('../mocks/users');
 module.exports = {
   listUsers(request, response) {
 
@@ -40,5 +40,39 @@ module.exports = {
 
       response.send(200, newUser);
 
+  },
+
+  updateUser(request, response) {
+    let { id } = request.params;
+    const { name } = request.body;
+
+    id = Number(id);
+
+    const userExists = users.find((user) => user.id === id);
+
+    if(!userExists) {
+      return response.send(400, {error: 'User not found'});
+    }
+
+    users = users.map((user)=> {
+      if(user.id === id) {
+        return{
+          ...user,
+          name,
+        };
+      }
+
+      return user;
+    });
+
+    response.send(200, { id, name });
+  },
+
+  deleteUser(request, response) {
+    let { id } = request.params;
+    id = Number(id);
+
+    users = users.filter((user) => user.id !== id);
+    response.send(200, { deleted: true });
   }
 }
