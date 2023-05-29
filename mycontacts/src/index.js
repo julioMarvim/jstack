7e9ef8aec3440169d9202f00e14a83f12399d43f
julioMarvim <juliomarvim@yahoo.com.br>
2023-05-29 16:40:13 -0300

    feat: cria servidor express

---
+++ mycontacts/src/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+
+const app = express();
+app.get('/', (request, response) => {
+  response.send('Hello workd!');
+});
+
+app.listen(3000, () => console.log('Server started at http://localhost:3000'));
