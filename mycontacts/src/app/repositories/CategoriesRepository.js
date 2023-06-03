6a328035e180a282d556672e002757743d40e1cc
julioMarvim <juliomarvim@yahoo.com.br>
2023-06-03 13:23:36 -0300

    feat: implementa metodo show

--- mycontacts/src/app/repositories/CategoriesRepository.js
+++ mycontacts/src/app/repositories/CategoriesRepository.js
@@ -1,11 +1,18 @@
 const db = require('../../database');
 
 class CategoriesRepository {
-  findAll() {
-    const rows = db.query(`
+  async findAll() {
+    const rows = await db.query(`
     SELECT * FROM categories ORDER BY name`);
 
     return rows;
+  }
+
+  async findById(id) {
+    const [row] = await db.query(`
+    SELECT * FROM categories
+    WHERE id = $1`, [id]);
+    return row;
   }
 
   async create({ name }) {
