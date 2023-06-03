415832ad2b903aac76608ea616c264d8a4a0b3fa
julioMarvim <juliomarvim@yahoo.com.br>
2023-06-03 13:08:16 -0300

    feat: cria metodos index e store

--- mycontacts/src/routes.js
+++ mycontacts/src/routes.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
-const contactController = require('./app/controllers/ContactController');
+const ContactController = require('./app/controllers/ContactController');
+const CategoryController = require('./app/controllers/CategoryController');
 
 const router = Router();
 
-router.get('/contacts', contactController.index);
-router.get('/contacts/:id', contactController.show);
-router.delete('/contacts/:id', contactController.delete);
-router.post('/contacts', contactController.store);
-router.put('/contacts/:id', contactController.update);
+router.get('/contacts', ContactController.index);
+router.get('/contacts/:id', ContactController.show);
+router.delete('/contacts/:id', ContactController.delete);
+router.post('/contacts', ContactController.store);
+router.put('/contacts/:id', ContactController.update);
+
+router.get('/categories', CategoryController.index);
+router.post('/categories', CategoryController.store);
 
 module.exports = router;
