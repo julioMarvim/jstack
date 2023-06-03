5e1d73b4d643e9b68e9085cf45ee41cf316b439e
julioMarvim <juliomarvim@yahoo.com.br>
2023-06-03 12:24:25 -0300

    refactor: refatora update contacts

--- mycontacts/src/app/repositories/ContactsRepository.js
+++ mycontacts/src/app/repositories/ContactsRepository.js
@@ -52,28 +52,23 @@ class ContactsRepository {
     return row;
   }
 
+  async update(id, {
+    name, email, phone, category_id,
+  }) {
+    const [row] = await db.query(`
+    UPDATE contacts
+    SET name =  $1, email = $2, phone = $3, category_id = $4
+    WHERE id = $5
+    RETURNING *
+    `, [name, email, phone, category_id, id]);
+
+    return row;
+  }
+
   delete(id) {
     return new Promise((resolve) => {
       contacts = contacts.filter((contact) => contact.id !== id);
       resolve();
-    });
-  }
-
-  update(id, {
-    name, email, phone, category_id,
-  }) {
-    return new Promise((resolve) => {
-      const updatedContact = {
-        id,
-        name,
-        email,
-        phone,
-        category_id,
-      };
-      contacts = contacts.map((contact) => (
-        contact.id === id ? updatedContact : contact
-      ));
-      resolve(updatedContact);
     });
   }
 }
