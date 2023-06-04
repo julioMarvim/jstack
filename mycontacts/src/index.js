1e1e77dc5d3f45bce1a99b0d405605a7bc92cef8
julioMarvim <juliomarvim@yahoo.com.br>
2023-06-04 10:26:34 -0300

    feat: criando error handler

--- mycontacts/src/index.js
+++ mycontacts/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('express-async-errors');
 const routes = require('./routes');
 
 const app = express();
@@ -6,5 +7,10 @@ const app = express();
 requisições do tipo POST, PUT e PATCH */
 app.use(express.json());
 app.use(routes);
+app.use((error, request, response, next) => {
+  console.log('####Error Handler');
+  console.log(error);
+  response.sendStatus(500);
+});
 
 app.listen(3000, () => console.log('Server started at http://localhost:3000'));
